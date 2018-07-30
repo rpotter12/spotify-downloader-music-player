ae4c7ceeb5fe5a58a478b160ffd0468b0d9b273b
ROHIT POTTER <rohitpotter12@gmail.com>
2018-07-30 22:39:23 +0530

    updating files

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Spotify Downloader Music Player");
-        primaryStage.setScene(new Scene(root, 800, 675));
+        primaryStage.setScene(new Scene(root, 800, 475));
         primaryStage.show();
     }
 
