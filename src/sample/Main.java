40025e821849b65d2f1680ca06f585f3afb11064
Rohit Potter <rpotter12>
2018-07-31 21:12:14 +0530

    disabling the maximizing ability of the result

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Spotify Downloader Music Player");
         primaryStage.setScene(new Scene(root, 800, 475));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
