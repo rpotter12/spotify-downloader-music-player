2b1238b8aa7b6b6763ad3bc004b6de6400862c50
Rohit Potter <rpotter12>
2018-08-08 23:08:33 +0530

    whenever the music name textfield section is empty, the download button remains disable

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,12 +3,24 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import java.awt.*;
 import java.io.IOException;
 
 public class Controller {
 
     @FXML
     private GridPane rootPane;
+    @FXML
+    private Button download;
+    @FXML
+    private TextField musiclink;
+    @FXML
+    public void initialize()
+    {
+        download.setDisable(true);
+    }
     //Stage stage;
     public void homePressed() throws IOException {
         GridPane homepane = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -29,8 +41,13 @@ public class Controller {
         GridPane pane = FXMLLoader.load(getClass().getResource("about.fxml"));
         rootPane.getChildren().setAll(pane);
     }
-
-
+    @FXML
+    public void handleKeyReleased()
+    {
+        String text = musiclink.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        download.setDisable(disableButtons);
+    }
 
 }
 
